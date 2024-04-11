f263d184a10c7d0c114a961fdc0e92106ba711d4
DurgaMewada <mewadadurga83@gmail.com>
2024-04-11 23:50:29 +0530

    Commit

--- lib/utils/components/Project8.dart
+++ lib/utils/components/Project8.dart
@@ -1,11 +1,96 @@
 import 'package:flutter/material.dart';
-Align body8() {
-  return Align(
 
-  );
-}
+Scaffold Body8() {
+  return Scaffold(
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: 350,
+          width: 350,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(width: 40, color: Colors.orange),
+              borderRadius: BorderRadius.circular(350),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 10,
+                  spreadRadius: 3,
+                ),
+              ]),
+          // alignment: Alignment.center,
+          alignment: Alignment(0.2, -4),
 
-AppBar appBar8() {
-  return AppBar(
-  );
+          child: Container(
+            height: 270,
+            width: 270,
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              border:
+                  Border(bottom: BorderSide(color: Colors.white, width: 15)),
+              borderRadius: BorderRadius.circular(350),
+            ),
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 20, bottom: 50),
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(70),
+                    ),
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(60),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20, bottom: 50),
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(70),
+                    ),
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(60),
+                      ),
+
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.orange,
+        child: Center(
+          child: Text(
+            'Emoji',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+          ),
+        ),
+      ),
+    );
 }
